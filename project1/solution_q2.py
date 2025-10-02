85e2cbe2951de791b81968d215d5ed656e00dbde
Akshaya Shyamsundar Rekha <akshayashyam@ibm.com>
2025-10-02 13:20:52 -0400

    added just a is_goal method and a methd for the step costs but have not finished it yet

--- project1/solution_q2.py
+++ project1/solution_q2.py
@@ -1,3 +1,6 @@
+import heapq
+from math import inf
+
 def read_input(file_name = "input.txt"):
 
     #reeading initial file from input.tct
@@ -13,6 +16,45 @@ def read_input(file_name = "input.txt"):
     parts = [p.strip() for p in line.split(",")]
     M_left, C_left, M_right, C_right, Boat = parts
     return (int(M_left), int(C_left), int(M_right), int(C_right), Boat)
+
+
+def is_goal(state):
+    #checking if goal state has been reached (all missionaries and cannibals are on the right side)
+    M_left, C_left, M_right, C_right, Boat = state 
+    return M_left == 0 and C_left == 0 #checking that missionaries and cannibals are both zero
+
+
+#calculates the cost of each step
+def step_cost(current, next, model):
+    """
+    Finding costs for both Cost model A and cost model B
+    Cost Model A:
+    - cost = 2 for each missionary
+    - cost = 1 for each cannibal
+
+    Cost Model A: cost by direction the boat is traveling in
+    - L->R costs 2
+    - R->L costs 1
+    """
+
+
+
+
+
+def uniform_cost():
+    """
+    starting state has zero code. then remove the next node from the list, which is currently technically the node with the minimum cost
+    if selected path is the target, it can be returning
+    when the boat = L, then we know the missionaries and cannibals are moving from left to right
+    when boat = r M and C or moving right to left
+    when computing the path (so next state) which is traveled, you also need to add on the 
+    the cost of each missionary nad each cannibal
+
+    """
+
+
+
+
 
 
 

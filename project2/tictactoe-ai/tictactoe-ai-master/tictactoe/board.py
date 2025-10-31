8512217f715a865f4da47990779054e88e0c8291
Akshaya Shyamsundar Rekha <akshayashyam@ibm.com>
2025-10-30 21:47:35 -0400

    Updated undo methodology, updating, and conditions for full board

--- project2/tictactoe-ai/tictactoe-ai-master/tictactoe/board.py
+++ project2/tictactoe-ai/tictactoe-ai-master/tictactoe/board.py
@@ -23,6 +23,8 @@ class Board:
         table (list[Symbol]): Container to store the current board state, use Square as index 
         turn (Symbol): Current turn to make a move
         win_conditions (list[list[Square]]): All possible connections to win the game
+        in_response_mode (bool): - Tracks if we're in response mode
+        get_first_three (Optional[Symbol]): - Who made the first 3-in-a-row
     """
 
     def __init__(self, size: int = 3):
@@ -198,22 +200,62 @@ class Board:
 
         Returns:
             Optional[Symbol]: Symbol of connected tiles if exists
+
+            In wild tic-tac-toe:
+            - If someone makes 3 in a row first, opponent gets to move n response (if they get that, they would win)
+            - Winner is only determined after response move is finished (so if the next player gets a 3 in a row, they win)
         """
-        connection = self.get_connection()
+        connection = self.get_first_three()
+
+        #if there are no 3 in a rows: so the length of connection is zero
         if len(connection) == 0:
+            #check if we are in the response mode, because the responder failed to get 3 in a row
+            if self.in_response_mode:
+                return self.get_first_three #whoever got the three in a row first 
             return None
-        elif self.square_value(connection[0]) == Symbol.CIRCLE:
-            return Symbol.CIRCLE
-        else:
-            return Symbol.CROSS
+
+        #a playerA  with three in a row
+        player_with_connection = self.square_value(connection[0])
+
+        #when in response mode, the second player successfully gets three in a row
+        if self.in_response_mode:
+            return player_with_connection
+
+        #the first player got a three in a row, but there is no winner
+        return None
+
+
+        #if there are no 3 in a rows: so the length of connection is zero
+        # if len(connection) == 0:
+        #     return None
+        # elif self.square_value(connection[0]) == Symbol.CIRCLE:
+        #     return Symbol.CIRCLE
+        # else:
+        #     return Symbol.CROSS
 
     def is_gameover(self) -> bool:
         """Check for gameover
 
         Returns:
             bool: True if there's winner or draw
+        
+        - i have added a check for in_response_mode and when the board has no more spots to fill, the game ends cause the next player cant make any other moves
+        - edge case of board full so no more responses possible
         """
-        return self.winner() is not None or self.is_draw()
+
+        if self.winner() is not None:
+            return True
+        
+        if self.is_draw():
+            return True
+
+        #case for when full board
+        if self.in_response_mode and len(self.empty_squares) == 0:
+            return True
+        
+        return False
+        
+        #return self.winner() is not None or self.is_draw()
 
     def _update(self):
         """Update the turn and score if there's winner
@@ -242,17 +284,34 @@ class Board:
                     self.turn = Symbol.CROSS
                 else:
                     self.turn = Symbol.CIRCLE #switching x's to o's
+        
+        #Second Case: NO three in a row has been made at this turn
+        else:
+            #in response mode (the other player has to make move) but the responder did not get three in a row
+            if self.in_response_mode:
+                #the player A wins because they made three in a row, so you leave response mode
+                self.in_response_mode = False
+            #no three in a row at all
+            else:
+                #switch players normally:
+                if self.turn == Symbol.CIRCLE: #switching o to x
+                    self.turn = Symbol.CROSS
+                else:
+                    self.turn = Symbol.CIRCLE #switching x's to o's
+    
+        #updating scores based on who has won the game or not
+        winner_symbol = self.winner()
+        if winner_symbol == Symbol.CIRCLE:
+            self.p1_score += 1
+        elif winner_symbol == Symbol.CROSS:
+            self.p2_score += 1
 
 
-
-
-
-
-        self.turn = Symbol.CROSS if self.turn == Symbol.CIRCLE else Symbol.CIRCLE #alternate the turn
-        if self.winner() == Symbol.CIRCLE:
-            self.p1_score += 1 #increment the score for the Ai
-        elif self.winner() == Symbol.CROSS:
-            self.p2_score += 1 #increment the score for the opponent
+        # self.turn = Symbol.CROSS if self.turn == Symbol.CIRCLE else Symbol.CIRCLE #alternate the turn
+        # if self.winner() == Symbol.CIRCLE:
+        #     self.p1_score += 1 #increment the score for the Ai
+        # elif self.winner() == Symbol.CROSS:
+        #     self.p2_score += 1 #increment the score for the opponent
 
     def push(self, square: Square, value: Symbol):
         """Store the symbol into the square
@@ -261,15 +320,32 @@ class Board:
             square (Square): square name
             value (Symbol): symbol
         """
-        self.table[square] = value
 
-    def undo(self, square: Square):
+        prev_state = {
+            'turn': self.turn,
+            'in_response_mode': self.in_response_mode,
+            'get_first_three': self.get_first_three
+        }
+
+        #updating the state 
+        self.table[square] = value #putting the symbol into a certain square
+        
+        return prev_state
+
+
+    def undo(self, square: Square, prev_state: dict):
         """Change the square's value to empty
 
         Args:
             square (Square): square name
         """
-        self.table[square] = Symbol.EMPTY
+        self.table[square] = Symbol.EMPTY #clearing the board
+
+        #restore the previous state
+        self.turn = prev_state['turn']
+        self.in_response_mode = prev_state['in_response_mode']
+        self.get_first_three = prev_state['first_three_connections_player']
+
 
     def move(self, square: Square):
         """Mark the square with symbol of current turn if valid and update the board

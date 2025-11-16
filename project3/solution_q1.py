8f4654f0ce1f7a9a496bb37492c93e2820b8b68c
Akshaya Shyamsundar Rekha <akshayashyam@ibm.com>
2025-11-16 17:23:46 -0500

    added hyperparameter variabes and created q table

--- project3/solution_q1.py
+++ project3/solution_q1.py
@@ -1,6 +1,31 @@
 import gymnasium as gym
+from collections import defaultdict
+import numpy as np
+import random
+
+
+
+#-------------
+#Hyperparameters
+#----------------
+NUM_EPISODES = 100000      #Number of training episodes
+ALPHA = 0.01               #Alpha - Learning rate (how much we update Q-values)
+GAMMA = 0.99               #Gamma - Discount factor (how much we value future rewards)
+EPSILON_START = 1.0        #Initial exploration rate
+EPSILON_MIN = 0.01         #Minimum exploration rate
+# DONT USE ITS OPTIONAL EPSILON_DECAY = 0.99995    # Decay rate per episode (optional)
+
+
+#black jack has two actions, either you stick or hit
+#for each state s, you need the Q value for hte state of either sticking or hitting
+
+
+
+
 env = gym.make('Blackjack-v1', natural=False, sab=False, render_mode= "human")
 ###YOUR Q-LEARNING CODE BEGINS
+
+
 
 
 
